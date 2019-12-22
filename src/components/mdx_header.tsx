2e1539645e0d3408848639189b2c8faff6ce7b93
cfcodefans <cf.chenfan@gmail.com>
2019-12-22 14:19:20 +0800

    ..

--- src/components/mdx_header.tsx
+++ src/components/mdx_header.tsx
@@ -7,7 +7,7 @@ function MDX_Header(mdMate: MarkdownMetaInfo): JSX.Element {
     console.info(mdMate)
     const meta: any = mdMate.meta
 
-    return (<div>
+    return (<header>
         <Head>
             <title>{mdMate.meta["title"] || HOME_TITLE}</title>
             <meta name="description" content={meta["desc"] || HOME_META_DESCRIPTION} />
@@ -15,7 +15,7 @@ function MDX_Header(mdMate: MarkdownMetaInfo): JSX.Element {
         </Head>
         <h1>{meta["title"]}</h1>
         <div>Created at: {mdMate.createdAt} | Modified at: {mdMate.modifiedAt}</div>
-    </div>)
+    </header>)
 }
 
 export default MDX_Header
