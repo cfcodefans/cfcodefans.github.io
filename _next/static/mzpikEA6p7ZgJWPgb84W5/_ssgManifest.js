54595187baf5259f4b14267292747bd00655d22d
cfcodefans <cf.chenfan@gmail.com>
2020-11-20 22:44:23 +0800

    open graph protocol testing

--- _next/static/mzpikEA6p7ZgJWPgb84W5/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fdigest","\u002Ftech\u002Fweb\u002Fmarkdown","\u002Ftech\u002Fweb\u002Freact-static","\u002Fstudy\u002Ffinance","\u002Fstudy","\u002Ftech","\u002Ftech\u002Fdata\u002Fh2db","\u002Ftech\u002Fweb","\u002Ftech\u002Fdata"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
