a40d3d4edc6fde0d7acb8762aeac16db21d01933
cfcodefans <cf.chenfan@gmail.com>
2020-11-30 18:27:48 +0800

    clean up

---
+++ _next/static/Sa0yt3-ETG1d5E5Ar4LaB/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Ftech\u002Fweb\u002Fhtml","\u002Ftech\u002Fdata","\u002Ftech","\u002Ftech\u002Fweb\u002Fmarkdown","\u002Ftech\u002Fweb","\u002Ftech\u002Fweb\u002Fssg","\u002Ftech\u002Fdata\u002Fdatabase","\u002Fdigest","\u002Fstudy","\u002Fstudy\u002Ffinance"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
