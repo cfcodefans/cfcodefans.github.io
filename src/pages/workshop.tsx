565d4bb368a1657590125e763886ccb579068e80
cfcodefans <cf.chenfan@gmail.com>
2022-11-06 22:59:06 +0800

    ..

--- src/pages/workshop.tsx
+++ src/pages/workshop.tsx
@@ -1,25 +1,32 @@
 import { useProperVisRect } from "components/algo-ds-vis/vis-utils"
-import { CSSProperties } from "react"
+import { CSSProperties, useState } from "react"
 import { useMedia } from "react-use"
-import { VictoryAxis, VictoryChart, VictoryTickStyleObject } from "victory"
+import { ScatterSymbolType, VictoryAxis, VictoryChart, VictoryScatter, VictoryTickStyleObject } from "victory"
 const FILE_NAME: string = "workshop.tsx"
-
 
 const _grid: CSSProperties = { stroke: "lightgrey", strokeWidth: 1 }
 const _tick: VictoryTickStyleObject = { stroke: "black", strokeWidth: 1, size: 5 }
 
-export default function Workshop(): JSX.Element {
-    // const classes = useStyles()
+declare type TScatterData = {
+    x: number
+    y: number
+    label?: string
+    symbol?: ScatterSymbolType
+}
 
+function ScatterGen(): JSX.Element {
     let measureRect = useProperVisRect(4 / 3.0)
     let { width, height } = measureRect
     let orientation = useMedia("(orientation: portrait)")
 
-    let cmp: JSX.Element = <div className="d-block bg-light rounded" style={{ width: `${width}px`, height: `${height}px` }}  >
+    let [scatter, setScatter] = useState([])
+
+    return <div className="d-block bg-light rounded" style={{ width: `${width}px`, height: `${height}px` }}  >
         <VictoryChart width={width} height={height}>
             <VictoryAxis
                 standalone={false}
                 width={width} height={1}
+                label="whatever X"
                 domain={[0, width / 30]}
                 tickCount={width / 30}
                 style={{ ticks: _tick, grid: _grid }}
@@ -29,12 +36,22 @@ export default function Workshop(): JSX.Element {
                 dependentAxis={true}
                 standalone={false}
                 width={1} height={width}
+                label="whatever Y"
                 domain={[0, height / 30]}
                 tickCount={height / 30}
                 style={{ ticks: _tick, grid: _grid }}
                 orientation="left" />
+
+            <VictoryScatter
+                style={{ data: { fill: "#c43a31" } }}
+                size={5}
+                data={scatter}
+            />
         </VictoryChart>
     </div>
+}
 
-    return cmp
+export default function Workshop(): JSX.Element {
+    // const classes = useStyles()
+    return <ScatterGen />
 }
